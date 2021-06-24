532a3c027b768f7c10551c9ff0c673a53a22290b
Tim Bauer <tim.bauer@gmx.us>
2021-06-24 13:36:27 +0200

    Hide and change dev key

--- src/farmdb/settings.py
+++ src/farmdb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rj09ptqslo5o8&j)c6bh7r3y0ao!3cd%s(o)17&9+inw*efn1y'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
