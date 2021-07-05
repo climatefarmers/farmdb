866c7951e73a8f4f2d85dd4433f3e820979c500f
Tim Bauer <tim.bauer@gmx.us>
2021-07-05 16:03:58 +0200

    Nested serializer for survey input

--- src/farmdb_core/serializers.py
+++ src/farmdb_core/serializers.py
@@ -1,24 +1,48 @@
 from rest_framework import serializers
-from .models import Person, Farm, PersonToRoleToOrg, SurveyAnswers
+from address.models import Country, State, Locality, Address
+from .models import (Organization, Role, Person, Farm, PersonToRoleToOrg,
+                     SurveyAnswers, CommunicationPreferences)
 from .utils.survey.parser import parse_survey
 
-class PersonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Person
-        fields = ['first_name', 'last_name', 'email', 'phone',
-                  'address', 'comm_pref']
 
-class RoleSerializer(serializers.ModelSerializer):
+class CountrySerializer(serializers.ModelSerializer):
     class Meta:
-        model = PersonToRoleToOrg
-        fields = ['person', 'role', 'organization']
+        model = Country
+        fields = ['name']
+        extra_kwargs = {
+            'name': {'validators':[]} # We're maintaining uniqueness through get_or_create further down
+        }
 
-class FarmSerializer(serializers.ModelSerializer):
+
+class StateSerializer(serializers.ModelSerializer):
+    country = CountrySerializer()
+
     class Meta:
-        model = Farm
-        fields = ['name', 'description', 'website', 'address',
-                  'farm_size_approx', 'date_joined', 'public_profile',
-                  'survey_answers']
+        model = State
+        fields = ['name', 'country']
+
+
+class LocalitySerializer(serializers.ModelSerializer):
+    state = StateSerializer()
+
+    class Meta:
+        model = Locality
+        fields = ['name', 'state']
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    locality = LocalitySerializer()
+
+    class Meta:
+        model = Address
+        fields = ['street_number', 'route', 'raw', 'locality']
+
+
+class CommunicationPreferencesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommunicationPreferences
+        fields = ['comm_channel']
+
 
 class SurveyAnswersSerializer(serializers.ModelSerializer):
     class Meta:
@@ -28,12 +52,94 @@ class SurveyAnswersSerializer(serializers.ModelSerializer):
                   'uses_icides', 'receives_funding']
 
 
-class FarmerSurveySerializer(serializers.ModelSerializer):
-    person = PersonSerializer()
-    role = RoleSerializer()
-    farm = FarmSerializer()
+class PersonSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+    comm_pref = CommunicationPreferencesSerializer()
+
+    class Meta:
+        model = Person
+        fields = ['first_name', 'last_name', 'email', 'phone',
+                  'address', 'comm_pref']
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ['role']
+
+
+class FarmSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
     survey_answers = SurveyAnswersSerializer()
 
+    class Meta:
+        model = Farm
+        fields = ['name', 'website', 'address',
+                  'farm_size_approx', 'survey_answers']
+
+
+class PersonToRoleToOrgSerializer(serializers.ModelSerializer):
+    person = PersonSerializer()
+    role = RoleSerializer()
+    organization = FarmSerializer()
+
+    class Meta:
+        model = PersonToRoleToOrg
+        fields = ['person', 'role', 'organization']
+
+
     def create(self, validated_data: dict):
-        parsed = parse_survey(validated_data)
-        pass
+
+        address = validated_data['person'].pop('address')
+        validated_data['organization'].pop('address')
+        comm_pref = validated_data['person'].pop('comm_pref')
+        survey_answers = validated_data['organization'].pop('survey_answers')
+
+        country, _ = Country.objects.get_or_create(
+            name=address['locality']['state']['country']['name'],
+            code=''
+        )
+        state, _ = State.objects.get_or_create(
+            name=address['locality']['state']['name'],
+            code='',
+            country=country
+        )
+        locality, _ = Locality.objects.get_or_create(
+            name=address['locality']['name'],
+            postal_code='',
+            state=state
+        )
+        address, _ = Address.objects.get_or_create(
+            street_number=address['street_number'],
+            route=address['route'],
+            raw=address['raw'],
+            locality=locality
+        )
+
+        comm_pref, _ = CommunicationPreferences.objects.get_or_create(
+            comm_channel=comm_pref['comm_channel'])
+        
+        person, _ = Person.objects.get_or_create(
+            **validated_data['person'], 
+            address=address,
+            comm_pref=comm_pref
+        )
+
+
+        survey_answers, _ = SurveyAnswers.objects.get_or_create(**survey_answers)
+
+        farm, _ = Farm.objects.get_or_create(
+            **validated_data['organization'],
+            address=address,
+            survey_answers=survey_answers
+        )
+
+        role, _ = Role.objects.get_or_create(**validated_data['role'])  # 1 for Farmer
+
+        person_to_role_to_org = PersonToRoleToOrg.objects.create(
+            person=person,
+            role=role,
+            organization=farm
+        )
+
+        return person_to_role_to_org
--- src/farmdb_core/views.py
+++ src/farmdb_core/views.py
@@ -3,14 +3,22 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import FarmerSurveySerializer
+from .serializers import PersonToRoleToOrgSerializer
+from .utils.survey.parser import parse_survey
+
 # Create your views here.
 class TypeFormFarmerSurvey(APIView):
     """
     Receives a TypeForm Farmer Survey response through a webhook and parses it into FarmDB
     """
+
+    def get(self, request, pk, format=None):
+        return None
+
+
     def post(self, request, format=None):
-        serializer = FarmerSurveySerializer(data=request.data)
+        parsed_data = parse_survey(request.data)
+        serializer = PersonToRoleToOrgSerializer(data=parsed_data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
