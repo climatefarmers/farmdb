3386295a7e1f15e4464a51bdf3c44c68f137d259
Tim Bauer <tim.bauer@gmx.us>
2021-07-07 17:05:21 +0200

    Add test cases for address parsing

--- src/farmdb_core/utils/survey/test.py
+++ src/farmdb_core/utils/survey/test.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from .parser import parse_survey
+from .parser import parse_survey, split_street, MISSING_STREET_NR
 
 
 @pytest.fixture()
@@ -16,3 +16,16 @@ def test_survey_parser(survey_response_body):
     parsed = parse_survey(survey_response_body)
     assert parsed['person']['email'] == "an_account@example.com"
     assert parsed['person']['comm_pref']['comm_channel'] == 2
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ('Foobar Str. 345', ['345', 'Foobar Str.']),
+        ('6 Whatever Lane', ['6', 'Whatever Lane']),
+        ('Am alten Rathaus', [MISSING_STREET_NR, 'Am alten Rathaus'])
+    ]
+)
+def test_split_street(test_input, expected):
+    out = split_street(test_input)
+    assert out == expected
