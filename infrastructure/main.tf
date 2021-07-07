656fcc36cdec61851c49064da260a8c27b9e6c9e
Tim Bauer <tim.bauer@gmx.us>
2021-07-07 17:48:31 +0200

    Update region

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
      bucket = "farmdb-tfstate"
      key = "farmdb.tfstate"
-     region = "eu-west3"
+     region = "europe-west3"
      endpoint = "https://storage.googleapis.com"
      skip_credentials_validation = true
   }
@@ -11,14 +11,14 @@ terraform {
 
 provider "google" {
   project     = "idyllic-silicon-318213"
-  region      = "eu-west3"
+  region      = "europe-west3"
 }
 
 
 resource "google_sql_database_instance" "farmdb-postgres" {
   name             = "farmdb"
   database_version = "POSTGRES_11"
-  region           = "eu-west3"
+  region           = "europe-west3"
 
   settings {
     # Second-generation instance tiers are based on the machine
