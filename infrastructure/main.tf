7f053517939724e5c88bffe925ef7da117cdc076
Tim Bauer <tim.bauer@gmx.us>
2021-07-08 17:26:15 +0200

    Add kubernetes and node pool

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,19 +7,54 @@ terraform {
 
 
 provider "google" {
-  project     = "idyllic-silicon-318213"
-  region      = "europe-west3"
+  project     = var.gcs_project_id
+  region      = var.gcp_region
 }
 
 
 resource "google_sql_database_instance" "farmdb-postgres" {
-  name             = "farmdb"
-  database_version = "POSTGRES_11"
-  region           = "europe-west3"
+  name             = "${var.resource_group}-${var.postgres_db_name}-${var.env}"
+  database_version = var.postgres_version
+  region           = var.gcp_region
 
   settings {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
-    tier = "db-f1-micro"
+    tier = var.postgres_host_tier
+  }
+}
+
+
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "${var.resource_group}-${var.kubernetes_cluster_name}-${var.env}"
+  location = var.gcp_region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "${var.resource_group}-${var.kubernetes_cluster_name}-nodepool-${var.env}"
+  location   = var.gcp_region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.node_count
+
+  node_config {
+    preemptible  = true
+    machine_type = var.node_tier
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
 }
---
+++ infrastructure/variables.tf
@@ -0,0 +1,53 @@
+variable "env" {
+    description = "The environment to deploy"
+    default = "dev"
+}
+
+variable "resource_group" {
+    description = "A resource name prefix for grouping"
+    default = "cfcommon"
+}
+
+variable "gcp_region" {
+    description = "GCP region for deployments"
+    default = "europe-west3"
+}
+
+variable "gcs_project_id" {
+    description = "The GCS project to use"
+}
+
+# Postgres Variables
+variable "postgres_db_name" {
+    description = "Name for the postgres db"
+    default = "farmdb"
+}
+
+variable "postgres_version" {
+    description = "Postgres version to use"
+    default = "POSTGRES_11"
+}
+
+variable "postgres_host_tier" {
+    description = "The tier of the machine to host postgres"
+    default = "db-f1-micro"
+}
+
+# Kubernetes Variables
+
+variable "kubernetes_cluster_name" {
+    description = "Name for the kubernetes cluster"
+    default = "atlas"
+}
+
+# Node Pool Variables
+
+variable "node_count" {
+    description = "The default number of nodes"
+    default = 1
+}
+
+variable "node_tier" {
+    description = "The tier of the machines in the node pool"
+    default = "e2-small"
+}
