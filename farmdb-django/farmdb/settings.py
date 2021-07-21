8051e685e7356b0b49d943e07ba942e25161249c
Tim Bauer <tim.bauer@gmx.us>
2021-07-21 17:36:27 +0200

    Disable debug

--- farmdb-django/farmdb/settings.py
+++ farmdb-django/farmdb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
