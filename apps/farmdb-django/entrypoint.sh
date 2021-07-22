4f394c5bf43b41349de8ca329d7066e1103e79f4
Tim Bauer <tim.bauer@gmx.us>
2021-07-22 15:37:23 +0200

    auto yes

--- apps/farmdb-django/entrypoint.sh
+++ apps/farmdb-django/entrypoint.sh
@@ -2,5 +2,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py makemigrations farmdb_core
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
