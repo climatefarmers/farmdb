5c8e3658e47cc3d1764a72593b25559fe8a62d19
Tim Bauer <tim.bauer@gmx.us>
2021-07-23 12:59:41 +0200

    source control migrations

--- apps/farmdb-django/entrypoint.sh
+++ apps/farmdb-django/entrypoint.sh
@@ -1,6 +1,1 @@
-python manage.py makemigrations
-python manage.py migrate
-python manage.py makemigrations farmdb_core
-python manage.py migrate
-python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
