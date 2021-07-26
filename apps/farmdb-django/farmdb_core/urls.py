0e0510ace894956cd4a882b59a048d5a04dd9a53
Tim Bauer <tim.bauer@gmx.us>
2021-07-26 15:11:04 +0200

    FE prototype with simple map view

--- apps/farmdb-django/farmdb_core/urls.py
+++ apps/farmdb-django/farmdb_core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import TypeFormFarmerSurvey
+from . import views
 
 urlpatterns = [
-    path('farmsurveyresponse/', TypeFormFarmerSurvey.as_view())
+    path('farmsurveyresponse/', views.TypeFormFarmerSurvey.as_view()),
+    path('', views.index, name="home"),
+    path('map/', views.FieldsMapView.as_view()),
+    path('farms/', views.FarmsView.as_view(), name="farms"),
+    path('farms/<int:pk>/', views.FarmDetail.as_view(), name='farm_detail'),
 ]
--- apps/farmdb-django/farmdb_core/views.py
+++ apps/farmdb-django/farmdb_core/views.py
@@ -1,33 +1,77 @@
-from django.http.response import JsonResponse
-from django.shortcuts import render
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, authentication, permissions
-from .serializers import PersonToRoleToOrgSerializer
-from .utils.survey.parser import parse_survey
+import hmac
+import json
 from base64 import b64encode
 from hashlib import sha256
-import hmac
+
 from django.conf import settings
-#import settings
+from django.core.serializers import serialize
+from django.http import Http404, HttpResponse
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from rest_framework import authentication, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.views import generic
+
+from .models import Field, Farm
+from .serializers import PersonToRoleToOrgSerializer
+from .utils.survey.parser import parse_survey
+
+
+
+class FarmsView(generic.ListView):
+    template_name = "farms.html"
+    model = Farm
+
+
+class FarmDetail(generic.DetailView):
+    template_name = "farm_detail.html"
+    model = Farm
+
+
+
+    def get_context_data(self, **kwargs):
+        """Return the view context data."""
+        context = super().get_context_data(**kwargs)
+        farm_fields = Field.objects.filter(farm=context['farm'])
+        context["fields"] = json.loads(serialize("geojson", farm_fields))
+        print(context)
+        return context
+
+
+class FieldsMapView(TemplateView):
+    """Fields map view."""
+
+    template_name = "map.html"
+
+    def get_context_data(self, **kwargs):
+        """Return the view context data."""
+        context = super().get_context_data(**kwargs)
+        context["fields"] = json.loads(serialize("geojson", Field.objects.all()))
+        return context
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the FarmDB index.")
+
 
 # Create your views here.
 class TypeFormFarmerSurvey(APIView):
     """
     Receives a TypeForm Farmer Survey response through a webhook and parses it into FarmDB
     """
+
     authentication_classes = []
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, pk, format=None):
         return None
 
-
     def post(self, request, format=None):
         if valid_typeform_signature(request):
             parsed_data = parse_survey(request.data)
-            #TODO: try/except to store request body and stacktrace to debug db table
+            # TODO: try/except to store request body and stacktrace to debug db table
             serializer = PersonToRoleToOrgSerializer(data=parsed_data)
             if serializer.is_valid():
                 serializer.save()
@@ -37,11 +81,10 @@ class TypeFormFarmerSurvey(APIView):
 
 
 def valid_typeform_signature(request):
-    api_signature = request.META.get('HTTP_TYPEFORM_SIGNATURE')
+    api_signature = request.META.get("HTTP_TYPEFORM_SIGNATURE")
     secret = settings.FARMDB_CORE_TYPEFORM_SECRET
     computed_sig = hmac.new(
-        secret.encode('utf-8'),
-        msg=request.body, digestmod=sha256
+        secret.encode("utf-8"), msg=request.body, digestmod=sha256
     ).digest()
-    signature = 'sha256=' + b64encode(computed_sig).decode()
+    signature = "sha256=" + b64encode(computed_sig).decode()
     return api_signature == signature
