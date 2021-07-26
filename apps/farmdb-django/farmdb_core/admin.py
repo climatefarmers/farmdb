3c46221d8da96f29312f8edbfa458d300fd1a340
Tim Bauer <tim.bauer@gmx.us>
2021-07-26 15:09:37 +0200

    Begin customizing admin field

--- apps/farmdb-django/farmdb_core/admin.py
+++ apps/farmdb-django/farmdb_core/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from django.contrib.gis import admin as gisadmin
 from .models import Field, Farm, SurveyAnswers, Person
-# Register your models here.
+@admin.register(Field)
+class FieldAdmin(gisadmin.OSMGeoAdmin):
+    fields = ['field_name','geom', 'farm']
+    map_info = True
 
-#class FieldAdmin(admin.ModelAdmin):
-#    fields = ['field_name','geom']
+    layers = (
+        ('maps', 1),
+        ('photos', 0.3),
+    )
 
-admin.site.register(Field, gisadmin.OSMGeoAdmin)
+    #def get_form(self, request, obj=None, **kwargs):
+    #    """
+    #    Use special form during foo creation
+    #    """
+    #    defaults = {}
+    #    if obj is None:
+    #        defaults['form'] = self.custom_form
+    #    defaults.update(kwargs)
+    #    print(defaults)
+    #    return super().get_form(request, obj, **defaults)
+    
+
 admin.site.register(Farm, admin.ModelAdmin)
 admin.site.register(SurveyAnswers, admin.ModelAdmin)
 admin.site.register(Person, admin.ModelAdmin)
